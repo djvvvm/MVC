ecd74c92edd5ceaa4e67e1d8cb9d55e6f06fce49
Денис <denis@denys.local>
2024-11-13 13:40:33 +0200

    Testing jenkins

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,3 @@ tasks.register<Exec>("testApp") {
     group = "verification"
     description = "Run Python unit tests"
     commandLine("python3", "-m", "unittest", "discover", "-s", "tests")
-}
-
-tasks.register("build") {
-    group = "build"
-    description = "Custom build task that depends on tests"
-    dependsOn("testApp")
-}
-
-tasks.register("clean") {
-    group = "build"
-    description = "Custom clean task to delete build directories"
-    doLast {
-        delete("build")
-    }
-}
